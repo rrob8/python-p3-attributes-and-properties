c5bbc127e61207944f432a790c42c79921ea42a9
Robert Bubnik <rbubnik@gmail.com>
2024-04-25 22:12:47 -0400

    passing all tests

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self, name = 'Test' , breed = 'Pug'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def get_breed(self):
+        return self._breed
+
+    def set_name(self, name):
+        if (type(name) is str) and ( 1 <= len(name) <= 25):
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")  
+
+
+    def set_breed(self, breed):
+        approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+        if(type(breed) is str) and (breed in approved_breeds):
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds." )
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
